c6c422c3681d4485ec2e856633b9b6f02491e754
Pushkal Katara <katarapushkal@gmail.com>
2017-12-22 02:08:30 +0530

    DepthActionUpdate

---
+++ src/alpheus_actions/clients/depthClient.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+from __future__ import print_function
+import rospy
+import actionlib
+import alpheus_actions.msg
+
+def depthClient():
+    client = actionlib.SimpleActionClient('depthServer', \
+    alpheus_actions.msg.depthAction)
+    client.wait_for_server()
+    goal = alpheus_actions.msg.depthGoal(depth_setpoint=520)
+    client.send_goal(goal)
+    client.wait_for_result()
+    return client.get_result()
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('depthClient')
+        result = depthClient()
+        print(result)
+    except rospy.ROSInterruptException:
+        print("Program interrupted before completetion", file=sys.stderr)
---
+++ src/alpheus_actions/servers/depthServer.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python
+import rospy
+import actionlib
+import alpheus_actions.msg
+from alpheus_msgs.msg import pressure
+
+class depthAction(object):
+    _feedback = alpheus_actions.msg.depthFeedback()
+    _result = alpheus_actions.msg.depthResult()
+
+    def __init__(self, name):
+        rospy.Subscriber("/pressure", pressure, self.pressure_cb)
+        self._da = name
+        self._ds = actionlib.SimpleActionServer(
+            self._da, \
+            alpheus_actions.msg.depthAction, \
+            execute_cb = self.depthCallback, \
+            auto_start = False)
+        self._ds.start()
+
+    def pressure_cb(data):
+        self._pressure = data.pressure
+
+    def depthCallback(self, goal):
+        r = rospy.Rate(1)
+        success = True
+
+        self._feedback.depth_error = _pressure - goal.depth_setpoint
+        rospy.loginfo('%s : Going to Pressure %f with current Pressure : %f',\
+            self._da , \
+            goal.depth_setpoint, \
+            _feedback.depth_error)
+
+        while(goal.depth_setpoint != _pressure):
+            if self._ds.is_preempt_requested():
+                rospy.loginfo('%s : Preempted' % self._da)
+                self._ds.set_preempted()
+                success = False
+                break
+            self._feedback.depth_error = self._pressure
+            self._ds.publish_feedback(self._feedback)
+            r.sleep()
+
+        if success:
+            self._result.depth_final = self._feedback.depth_error
+            rospy.loginfo('%s : Success' % self._da)
+            self._ds.set_succeeded(self._result)
+
+if __name__ == '__main__':
+    rospy.init_node('depthServer')
+    server = depthAction(rospy.get_name())
+    rospy.spin()
