4e94a6fc3e345d5ea5af88ffc1aa63d8dfcfb3e5
Pushkal Katara <katarapushkal@gmail.com>
2018-03-02 02:47:31 +0530

    GUI Video

--- src/gui/scripts/UIs/VisionUI/video.py
+++ src/gui/scripts/UIs/VisionUI/video.py
@@ -13,8 +13,9 @@ import datetime
 import imutils
 import cv2
 import rospy
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
 
 '''
 To Do -
@@ -26,7 +27,7 @@ class video:
     def __init__(self, frameVideo, topic, row, column, name):
         self.row = row
         self.column = column
-        rospy.Subscriber(topic, Image, self.image_cb)
+        rospy.Subscriber(topic, CompressedImage, self.image_cb)
         self.bridge = CvBridge()
         frame = Frame(frameVideo, bg='white')
         frame.grid(row=self.row, column=self.column, sticky=W, padx=15, pady=15)
@@ -37,8 +38,12 @@ class video:
 
     def image_cb(self, data):
         try:
+            np_arr = np.fromstring(data.data, np.uint8)
+            self.image_cv = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+            '''
             image = self.bridge.imgmsg_to_cv2(data, "bgr8")
             self.image_cv = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            '''
             image = imutils.resize(self.image_cv, width=300)
             image = PIL.Image.fromarray(image)
             image = PIL.ImageTk.PhotoImage(image)
